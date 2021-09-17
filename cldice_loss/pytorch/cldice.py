861f9a0a2c354d82777357d81ba6c82dce3fbfdf
Torch-Fan <40243569+QiliangFan@users.noreply.github.com>
2021-09-17 08:44:24 +0800

    Update to binary segmentation

--- cldice_loss/pytorch/cldice.py
+++ cldice_loss/pytorch/cldice.py
@@ -10,11 +10,11 @@ class soft_cldice(nn.Module):
         self.iter = iter_
         self.smooth = smooth
 
-    def forward(y_true, y_pred):
-        skel_pred = soft_skel(y_pred, iters)
-        skel_true = soft_skel(y_true, iters)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,1:,...])+smooth)/(torch.sum(skel_pred[:,1:,...])+smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,1:,...])+smooth)/(torch.sum(skel_true[:,1:,...])+smooth)    
+    def forward(self, y_pred, y_true):
+        skel_pred = soft_skel(y_pred, self.iter)
+        skel_true = soft_skel(y_true, self.iter)
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,0,...])+self.smooth)/(torch.sum(skel_pred[:,0,...])+self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,0:,...])+self.smooth)/(torch.sum(skel_true[:,0,...])+self.smooth)    
         cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
         return cl_dice
 
@@ -30,23 +30,23 @@ def soft_dice(y_true, y_pred):
         [float32]: [loss value]
     """
     smooth = 1
-    intersection = torch.sum((y_true * y_pred)[:,1:,...])
-    coeff = (2. *  intersection + smooth) / (torch.sum(y_true[:,1:,...]) + torch.sum(y_pred[:,1:,...]) + smooth)
+    intersection = torch.sum((y_true * y_pred)[:,0,...])
+    coeff = (2. *  intersection + smooth) / (torch.sum(y_true[:,0,...]) + torch.sum(y_pred[:,0,...]) + smooth)
     return (1. - coeff)
 
 
 class soft_dice_cldice(nn.Module):
     def __init__(self, iter_=3, alpha=0.5, smooth = 1.):
-        super(soft_cldice, self).__init__()
+        super().__init__()
         self.iter = iter_
         self.smooth = smooth
         self.alpha = alpha
 
-    def forward(y_true, y_pred):
+    def forward(self, y_pred, y_true):
         dice = soft_dice(y_true, y_pred)
         skel_pred = soft_skel(y_pred, self.iter)
         skel_true = soft_skel(y_true, self.iter)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,1:,...])+self.smooth)/(torch.sum(skel_pred[:,1:,...])+self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,1:,...])+self.smooth)/(torch.sum(skel_true[:,1:,...])+self.smooth)    
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,0,...])+self.smooth)/(torch.sum(skel_pred[:,0,...])+self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,0,...])+self.smooth)/(torch.sum(skel_true[:,0,...])+self.smooth)    
         cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
         return (1.0-self.alpha)*dice+self.alpha*cl_dice
